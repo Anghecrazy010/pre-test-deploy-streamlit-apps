d9945ba93575bad8604531652b8ab23e05e24fcb
Anghelo R.A <99212410+Anghecrazy010@users.noreply.github.com>
2025-05-28 11:37:20 -0700

    se cambio el redirect uri

--- dev_dash_pedidos_cancelados.py
+++ dev_dash_pedidos_cancelados.py
@@ -22,7 +22,7 @@ st.set_page_config(
 AUTH0_CLIENT_ID = os.environ["AUTH0_CLIENT_ID"]
 AUTH0_CLIENT_SECRET = os.environ["AUTH0_CLIENT_SECRET"]
 AUTH0_DOMAIN = os.environ["AUTH0_DOMAIN"]
-REDIRECT_URI = "https://pedidos-cancelados-test.netlify.app/"
+REDIRECT_URI = "https://pre-test-deploy-streamlit-apps.onrender.com"
 
 # --- URLs de Auth0 ---
 AUTH0_AUTHORIZE_URL = f"https://{AUTH0_DOMAIN}/authorize"
